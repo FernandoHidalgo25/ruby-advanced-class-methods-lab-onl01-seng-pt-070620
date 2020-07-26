1a3c983e73f3c7fcb9e16e5466a61ca86710238e
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-26 21:00:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,13 +10,14 @@ class Song
     self.class.all << self
   end
   
-  def self.create
+<<<<<<< HEAD
+    def self.create
     song = self.new
     song.save
     song
   end 
 
-  def self.new_by_name(name)
+    def self.new_by_name(name)
     song = self.new
     song.name = name
     song
